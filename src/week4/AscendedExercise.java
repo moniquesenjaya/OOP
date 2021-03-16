e31cd56e11dd70b30a4335b897f8842f32e976eb
moniquesenjaya <monique.senjaya@binus.ac.id>
2021-03-16 23:25:28 +0700

    week four work extended

--- src/week4/AscendedExercise.java
+++ src/week4/AscendedExercise.java
@@ -20,31 +20,61 @@ public class AscendedExercise {
         int column2 = sc.nextInt();
         sc.nextLine();
 
-        if (column1 != row2){
-            System.out.println("The matrix cannot be multiplied");
-        }else{
-            int[][] a = new int[row1][column1];
-            int[][] b = new int[row2][column2];
+        System.out.print("Choose what to do (1)Dot (2)Mult: ");
+        int choice = sc.nextInt();
+        sc.nextLine();
 
-            for (int i = 0; i < a.length; i++) {
-                for (int j = 0; j < a[i].length; j++) {
-                    System.out.print("Enter the number of first matrix [" + (i + 1) + " , " + (j + 1) + "]: ");
-                    a[i][j] = sc.nextInt();
-                    System.out.println();
+        if (choice == 1){
+            if (column1 != row2){
+                System.out.println("The matrix cannot be multiplied");
+            }else{
+                int[][] a = new int[row1][column1];
+                int[][] b = new int[row2][column2];
+
+                for (int i = 0; i < a.length; i++) {
+                    for (int j = 0; j < a[i].length; j++) {
+                        System.out.print("Enter the number of first matrix [" + (i + 1) + " , " + (j + 1) + "]: ");
+                        a[i][j] = sc.nextInt();
+                        System.out.println();
+                    }
                 }
-            }
 
-            for (int i = 0; i < b.length; i++) {
-                for (int j = 0; j < b[i].length; j++) {
-                    System.out.print("Enter the number of second matrix [" + (i + 1) + " , " + (j + 1) + "]: ");
-                    b[i][j] = sc.nextInt();
-                    System.out.println();
+                for (int i = 0; i < b.length; i++) {
+                    for (int j = 0; j < b[i].length; j++) {
+                        System.out.print("Enter the number of second matrix [" + (i + 1) + " , " + (j + 1) + "]: ");
+                        b[i][j] = sc.nextInt();
+                        System.out.println();
+                    }
                 }
-            }
+                mDot(a,b, row1, column2);
 
-            mDot(a, b, row1, column2);
-            mMult(a,b, row1, column2);
+            }
+        }else if (choice == 2){
+            if (column1 != column2 || row1 != row2){
+                System.out.println("The matrix cannot be multiplied");
+            }else{
+                int[][] a = new int[row1][column1];
+                int[][] b = new int[row2][column2];
+
+                for (int i = 0; i < a.length; i++) {
+                    for (int j = 0; j < a[i].length; j++) {
+                        System.out.print("Enter the number of first matrix [" + (i + 1) + " , " + (j + 1) + "]: ");
+                        a[i][j] = sc.nextInt();
+                        System.out.println();
+                    }
+                }
+
+                for (int i = 0; i < b.length; i++) {
+                    for (int j = 0; j < b[i].length; j++) {
+                        System.out.print("Enter the number of second matrix [" + (i + 1) + " , " + (j + 1) + "]: ");
+                        b[i][j] = sc.nextInt();
+                        System.out.println();
+                    }
+                }
+                mMult(a,b, row1, column2);
+            }
         }
+
 
 
 
@@ -55,34 +85,28 @@ public class AscendedExercise {
     }
 
     public static void mMult(int[][] a, int[][] b, int row1, int column2) {
-        System.out.println("Matrix multiplication");
         int[][] c = new int[row1][column2];
-        for (int i = 0; i < c.length; i++) {
-            for (int j = 0; j < c[i].length; j++) {
-                c[i][j] = 0;
-                for (int k = 0; k < c[i].length; k++) {
-                    c[i][j] += a[i][k] * b[k][j];
-                }
-                System.out.print(c[i][j] + " ");
-            }
-            System.out.println();//new line
-        }
-        System.out.println();
-    }
-
-    public static void mDot(int[][] a, int[][] b, int row1, int column2) {
-        int[][] c = new int[row1][column2];
-        System.out.println("Dot Matrix");
+        System.out.println("Matrix Multiplication");
         for (int i = 0; i < c.length; i++) {
             for (int j = 0; j < c[i].length; j++) {
                 int num = 0;
                 for (int k = 0; k<c[i].length; k++){
                     num += a[i][k] * b[k][j];
                 }
-                    System.out.print(num + " ");
-                }
-                System.out.println(" ");
+                System.out.print(num + " ");
             }
-
+            System.out.println(" ");
         }
     }
+
+    public static void mDot(int[][] a, int[][] b, int row1, int column2) {
+        System.out.println("Dot Matrix");
+        int[] c = new int[a.length];
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c.length; j++) {
+                c[i] += a[i][j] * b[i][j];
+            }
+            System.out.print(c[i] + " ");
+        }
+    }
+}
